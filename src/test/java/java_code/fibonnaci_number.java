7ba6f4a3361c22e252d08902cfd2b722ba289df5
Ronit Gaikwad <gaikwadronit@gmail.com>
2024-12-09 14:54:22 +0530

    add

--- src/test/java/java_code/fibonnaci_number.java
+++ src/test/java/java_code/fibonnaci_number.java
@@ -6,14 +6,14 @@ public class fibonnaci_number {
     public static void main(String[] args) {
         
         
-        int i,a=0,b=1;
+        int a=0,b=1;
         int c = 0;
         System.out.println("Enter the nummber:");
 
         Scanner scanner=new Scanner(System.in);
        int num= scanner.nextInt();
        
-       for( i=1;i<num;i++){
+       for( int i=1;i<num;i++){
            c=a+b;
            a=b;
            b=c;
