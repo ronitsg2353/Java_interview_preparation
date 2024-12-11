8f09320723b9d022ad716359233e636e0b4a4d56
Ronit Gaikwad <gaikwadronit@gmail.com>
2024-12-11 21:59:24 +0530

    add

---
+++ src/test/java/selenium_code/Assert.java
@@ -0,0 +1,27 @@
+package selenium_code;
+
+public class Assert {
+    public static void main(String[] args) {
+
+        Assert asser=new Assert();
+
+        //assertEqaul
+        asser.assertEqual(Expected,actual);
+
+        //assertNotEqual
+        asser.assertNotEqual(Expected,actual);
+
+        //null
+        asser.assertNull();
+
+        //not null
+        asser.assertNotNull();
+
+        //True
+        asser.assertTrue();
+
+        //false
+        asser.assertFalse();
+
+    }
+}
