699539a09b298b6182c2e4b19a5f8ade6a909ef9
Ronit Gaikwad <gaikwadronit@gmail.com>
2025-01-15 20:29:16 +0530

    add

--- src/test/java/java_code/extract_number_from_StringArray.java
+++ src/test/java/java_code/extract_number_from_StringArray.java
@@ -3,14 +3,12 @@ package java_code;
 public class extract_number_from_StringArray {
 
     public static void main(String[] args) {
-        String [] strarr={"pink123","green345","nature5676"};
-        int size=strarr.length;
+        String[] strarr = {"pink123", "green345", "nature5676"};
 
         for(String str:strarr){
-           String numbers= str.replaceAll("[^0-9]","");
-            if(!numbers.isEmpty()){
-                System.out.println(Integer.parseInt(numbers));
-            }
+            String str1=str.replaceAll("[^0-9]","");
+            System.out.println(str1);
         }
+
     }
 }
